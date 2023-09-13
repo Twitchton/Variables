0a0e591dcccdc330dc0c8479ccd009c3f4cf08b5
Twitchton <Twitchton@gmail.com>
2023-09-13 13:42:17 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Variables
+ Exploration of variables
---
+++ Variables/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Variables
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Variables:");
+            Console.WriteLine("----------------\n");
+
+            Console.WriteLine("Press any Key to exit.");
+            Console.ReadKey();
+            System.Environment.Exit(0);
+        }
+    }
+}
